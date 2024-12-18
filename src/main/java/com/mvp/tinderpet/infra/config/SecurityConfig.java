731925f5ba84204505827071bd72c426c982b442
Adailson <adailson65@gmail.com>
2024-12-18 03:01:44 -0300

    fix google oauth2

--- src/main/java/com/mvp/tinderpet/infra/config/SecurityConfig.java
+++ src/main/java/com/mvp/tinderpet/infra/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.mvp.tinderpet.infra.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
